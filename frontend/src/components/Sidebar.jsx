4dd506ce3d20d8a4b26111227fd5dfb01c8a35b0
avantika <namelyavantika@gmail.com>
2025-05-16 21:37:02 +0530

    fixing stuff

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,7 @@
+// can implement retractable navbar using setState hook
+
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronLeftIcon, ChevronRightIcon, RectangleStackIcon, ClipboardDocumentIcon } from '@heroicons/react/20/solid'
 
 const sidebarMenu = [
     { name: 'Dashboard', href: "#", current: false },
@@ -13,13 +16,17 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-function SidebarComponent() {
+export default function SidebarComponent() {
     return (
-        <div className="bg-gray-800 px-[20px] py-[10px] h-screen">
+        <div className="bg-gray-800 px-[20px] py-[10px] h-screen min-w-xs">
             <div className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 text-left flex flex-col'>
-                <h6 className="text-left text-gray-300 my-2">MENU</h6>
+                <div className='flex flex-row justify-evenly items-center'>
+                    <h6 className="align-middle text-left text-gray-300 my-2">MENU</h6><button className='p-1.5 rounded-lg bg-gray-800 hover:bg-gray-900'><ChevronLeftIcon className='w-8 align-left'></ChevronLeftIcon></button></div>
+
                 <div className="flex flex-col space-x-4">
-                    {sidebarMenu.map((item) => (
+                    <SidebarItem icon={<ClipboardDocumentIcon className='w-6'></ClipboardDocumentIcon>} text={"DASHBOARD"}></SidebarItem>
+                    <SidebarItem icon={<RectangleStackIcon className='w-6'></RectangleStackIcon>} text={"TASKS"}></SidebarItem>
+                    {/* {sidebarMenu.map((item) => (
                         <a
                             key={item.name}
                             href={item.href}
@@ -31,7 +38,7 @@ function SidebarComponent() {
                         >
                             {item.name}
                         </a>
-                    ))}
+                    ))} */}
                 </div>
             </div>
 
@@ -42,4 +49,11 @@ function SidebarComponent() {
     );
 }
 
-export default SidebarComponent
+function SidebarItem({ icon, text, active, alert }) {
+    return (
+        <a className={`flex flex-row items-center my-1 rounded-md cursor-pointer transition-colors group ${active ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800" : "hover: bg-idigo-50 text-white"}`}>
+            {icon}
+            <span className='text-xs px-3 py-2'>  {text}</span>
+        </a>
+    )
+}
--- frontend/src/pages/TaskPage.jsx
+++ frontend/src/pages/TaskPage.jsx
@@ -1,17 +1,32 @@
 import ButtonComponent from "../components/Button.jsx";
+import flatpickr from "flatpickr";
 
-const user = {};
+const fp = flatpickr("datetime", {})
 
-const divClasses = "rounded-lg bg-gray-800 h-[150px] flex justify-center items-center";
+const dailyMontlyClass = {}
+
+const inputClass = "py-3 px-2 my-2 w-full bg-gray-600 rounded-lg"
 
 const TaskPage = () => {
     return (
         <>
-            <div className="w-full h-full bg-gray-900 flex flex-col justify-start items-center">
-                <div className="p-3 m-3 w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-
+            <div className="w-full h-full bg-gray-900 flex flex-col justify-start items-center p-3">
+                <div className="p-3 m-3 w-full grid grid-cols-2 gap-4">
+                    <div className="col-span-1">
+                        <div className="h-12 bg-gray-800 rounded-lg flex flex-row justify-evenly items-center"><span>Daily</span><span>Monthly</span></div>
+                    </div>
+                    <div className="col-span-1">
+                        <h6 className="text-center">Create a task</h6>
+                        <div className="task-form flex flex-col items-center">
+                            <form action="POST" className="flex flex-col items-center">
+                                <input type="text" name="title" id="title" placeholder="Title" className={inputClass} />
+                                <textarea cols={30} placeholder="Description" className={inputClass + ""}></textarea>
+                                <input type="datetime" name="deadline" id="deadline" className={inputClass} />
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </div >
+            </div>
         </>
     )
 }
